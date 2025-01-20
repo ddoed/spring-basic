aa38f601449724950dced97b500043683e253dac
yimnabin <yimnabin20051022@gmail.com>
2025-01-20 10:40:47 +0900

    Add component scanning and filter annotations

---
+++ src/test/java/hello/core/scan/AutoAppConfigTest.java
@@ -0,0 +1,18 @@
+package hello.core.scan;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+
+class AutoAppConfigTest {
+    @Test
+    void basicScan() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = ac.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
